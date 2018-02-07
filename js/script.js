4734ec2dde13ae0aa21f47e051ea3651a37acf28
Alecto <fomenkoandrey@inbox.ru>
2018-02-07 21:09:56 +0200

    improved javascript

--- js/script.js
+++ js/script.js
@@ -5,9 +5,23 @@ var addToCart = document.querySelectorAll('.item-actions__cart');
 var fnCounter = function() {
   cartCounter++;
   cartCounterLabel.innerHTML = cartCounter;
-  if (cartCounter === 1) { cartCounterLabel.style.display = 'block' }
+  if (cartCounter === 1) {
+    cartCounterLabel.style.display = 'block'
+  }
+
+  var tempButton = this;
+  var restoreHTML = this.innerHTML; // простейшее исправление двойного клика
+  // var restoreHTML = 'Add to cart <i class="fa fa-shopping-cart" aria-hidden="true"></i>';
+  this.innerHTML = 'Added';
+  setTimeout(function() {
+    fnRestore(tempButton, restoreHTML);
+  }, 1000); //как вариант - setTimeout(fnRestore, 1000, tempButton, restoreHTML);
+};
+
+function fnRestore(elem, restore) {
+  elem.innerHTML = restore;
 }
 
 for(var i=0; i<addToCart.length; i++){
-  addToCart[i].onclick = fnCounter;
+  addToCart[i].addEventListener('click', fnCounter);
 }
