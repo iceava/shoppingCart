4428f6f757325f49e087f257399155b160eb7c12
Alecto <fomenkoandrey@inbox.ru>
2018-03-09 09:06:57 +0200

    JS price counter added.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var cartCounter = 0;
 var cartCounterLabel = document.getElementById('cart-counter');
+var cartPrice = 0;
 var addToCart = document.querySelectorAll('.item-actions__cart');
 
 var fnCounter = function() {
@@ -12,11 +13,18 @@ var fnCounter = function() {
   var tempButton = this;
   var restoreHTML = this.innerHTML; // простейшее исправление двойного клика
   // var restoreHTML = 'Add to cart <i class="fa fa-shopping-cart" aria-hidden="true"></i>';
-  this.innerHTML = 'Added';
+  this.innerHTML = 'Added ' + fnPriceCounter(this) + ' $';
   setTimeout(function() {
     fnRestore(tempButton, restoreHTML);
-  }, 1000); //как вариант - setTimeout(fnRestore, 1000, tempButton, restoreHTML);
+  }, 2000); //как вариант - setTimeout(fnRestore, 1000, tempButton, restoreHTML);
 };
+
+function fnPriceCounter(e) {
+  var tempPrice = e.parentElement.previousElementSibling.innerHTML;
+  tempPrice = tempPrice.replace('$','').replace(' <sup>','.').replace('</sup>','');
+  cartPrice = cartPrice + Number(tempPrice);
+  return cartPrice.toFixed(2);
+}
 
 function fnRestore(elem, restore) {
   elem.innerHTML = restore;
