e67d2da617d778ea76cca44c80c973ad2558bc00
Andrey <fomenkoandrey@inbox.ru>
2018-03-23 23:03:14 +0200

    JS price counter added.

--- js/script.js
+++ js/script.js
@@ -3,33 +3,38 @@ var cartCounterLabel = document.getElementById('cart-counter');
 var cartPrice = 0;
 var addToCart = document.querySelectorAll('.item-actions__cart');
 
+var fnPriceCounter = function (e) {
+  var tempPrice = e.parentElement.previousElementSibling.innerHTML;
+  tempPrice = tempPrice.replace('$','').replace(' <sup>','.').replace('</sup>','');
+  cartPrice = cartPrice + Number(tempPrice);
+  return cartPrice.toFixed(2);
+};
+
+var fnRestore = function (elem, restore) {
+  elem.innerHTML = restore;
+  elem.addEventListener('click', fnCounter);
+};
+
 var fnCounter = function() {
   cartCounter++;
   cartCounterLabel.innerHTML = cartCounter;
+
   if (cartCounter === 1) {
     cartCounterLabel.style.display = 'block'
   }
 
   var tempButton = this;
-  var restoreHTML = this.innerHTML; // простейшее исправление двойного клика
-  // var restoreHTML = 'Add to cart <i class="fa fa-shopping-cart" aria-hidden="true"></i>';
+  var restoreHTML = this.innerHTML;
   this.innerHTML = 'Added ' + fnPriceCounter(this) + ' $';
-  setTimeout(function() {
-    fnRestore(tempButton, restoreHTML);
-  }, 2000); //как вариант - setTimeout(fnRestore, 1000, tempButton, restoreHTML);
+  this.removeEventListener('click', fnCounter);
+
+  setTimeout(fnRestore, 2000, tempButton, restoreHTML); // сокращаем
+  // более очевидный, но длинный вариант
+  // setTimeout(function() {
+  //   fnRestore(tempButton, restoreHTML);
+  // }, 2000);
 };
 
-function fnPriceCounter(e) {
-  var tempPrice = e.parentElement.previousElementSibling.innerHTML;
-  tempPrice = tempPrice.replace('$','').replace(' <sup>','.').replace('</sup>','');
-  cartPrice = cartPrice + Number(tempPrice);
-  return cartPrice.toFixed(2);
-}
-
-function fnRestore(elem, restore) {
-  elem.innerHTML = restore;
-}
-
-for(var i=0; i<addToCart.length; i++){
+for(var i=addToCart.length; i--;){
   addToCart[i].addEventListener('click', fnCounter);
 }
