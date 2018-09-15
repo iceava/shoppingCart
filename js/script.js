8b9bc5b135eeb7c5869b9a51a58a2de02e39feec
Alecto <fomenkoandrey@inbox.ru>
2018-09-15 21:03:57 +0300

    Оптимизация кода. Использование let.

--- js/script.js
+++ js/script.js
@@ -1,62 +1,42 @@
 var cartCounter = 0;
 var cartPrice = 0;
 var cartCounterLabel = document.getElementById('cart-counter');
-var items = document.querySelector('.page-content');
+var buttonsContainer = document.querySelector('.page-content');
 
 
 var fnPriceCounter = function (elem) {
-  var tempPrice = elem.parentElement.previousElementSibling.innerHTML;
-  // регулярка, продвинутый вариант
-  tempPrice = tempPrice.replace(/^\$(\d+)\s\D+(\d+).*$/g, '$1.$2');
-  // простейший вариант через цепочку вызовов replace
-  // tempPrice = tempPrice.replace('$','').replace(' <sup>','.').replace('</sup>','');
-  cartPrice += +tempPrice;
+  let tempPrice = elem.parentElement.previousElementSibling.innerHTML;
+  cartPrice += +tempPrice.replace(/^\$(\d+)\s\D+(\d+).*$/g, '$1.$2');
+  buttonsContainer.removeEventListener('click', btnClickHandler);
   return 'Added ' + cartPrice.toFixed(2) + ' $';
 };
 
 var fnRestore = function (elem, restore) {
   elem.innerHTML = restore;
-  // вариант деактивации кнопки, альтернатива снятию события
-  // решение может работать самостоятельно, снятие и добавление события - не нужны
-  elem.disabled = false;
-  items.addEventListener('click', fnCounter);
+  elem.disabled = false;   // деактивации кнопки - альтернатива снятию события
+  buttonsContainer.addEventListener('click', btnClickHandler);
 };
 
-var fnCounter = function (e) {
+var btnClickHandler = function (e) {
   var target = e.target;
 
   if (target.classList.contains('item-actions__cart')) {
 
-    cartCounter++;
-    cartCounterLabel.innerHTML = cartCounter;
+    cartCounterLabel.innerHTML = ++cartCounter;
 
     if (cartCounter === 1) {
       cartCounterLabel.style.display = 'block'
     }
 
-    // вариант деактивации кнопки, альтернатива снятию события
-    // решение может работать самостоятельно, снятие и добавление события - не нужны
-    target.disabled = true;
+    target.disabled = true;  // деактивации кнопки - альтернатива снятию события
 
-    var tempButton = target;
-    var restoreHTML = target.innerHTML;
+    let tempButton = target;
+    let restoreHTML = target.innerHTML;
 
     target.innerHTML = fnPriceCounter(target);
 
-    items.removeEventListener('click', fnCounter);
-
     setTimeout(fnRestore, 2000, tempButton, restoreHTML);
-    // сокращаем
-    // более очевидный, но длинный вариант
-    // setTimeout(function() {
-    //   fnRestore(tempButton, restoreHTML);
-    // }, 2000);
   }
 };
 
-// назначение обработчика через родителя
-items.addEventListener('click', fnCounter);
-// самое простое решение - обработчик на кнопках
-// for(var i=addToCart.length; i--;){
-//   addToCart[i].addEventListener('click', fnCounter);
-// }
+buttonsContainer.addEventListener('click', btnClickHandler);
