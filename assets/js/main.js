e1e5ecc51430098dcf64c8f6b6ae20226edde90e
Alecto <fomenkoandrey@inbox.ru>
2018-10-31 19:30:45 +0200

    замена var --> let, function --> => + небольшая оптимизация кода.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,33 @@
-var cartCounter = 0;
-var cartPrice = 0;
-var cartCounterLabel = document.getElementById('cart-counter');
-var buttonsContainer = document.querySelector('.page-content');
+let cartCounter = 0,
+    cartPrice = 0,
+    cartCounterLabel = document.querySelector('#cart-counter'),
+    buttonsContainer = document.querySelector('.page-content');
 
-
-var fnPriceCounter = function (elem) {
+let fnPriceCounter = (elem) => {
   let tempPrice = elem.parentElement.previousElementSibling.innerHTML;
   cartPrice += +tempPrice.replace(/^\$(\d+)\s\D+(\d+).*$/g, '$1.$2');
   buttonsContainer.removeEventListener('click', btnClickHandler);
   return 'Added ' + cartPrice.toFixed(2) + ' $';
 };
 
-var fnRestore = function (elem, restore) {
+let fnRestore = (elem, restore) => {
   elem.innerHTML = restore;
-  elem.disabled = false;   // деактивации кнопки - альтернатива снятию события
   buttonsContainer.addEventListener('click', btnClickHandler);
 };
 
-var btnClickHandler = function (e) {
-  var target = e.target;
+let btnClickHandler = (e) => {
+  let target = e.target;
 
   if (target.classList.contains('item-actions__cart')) {
 
-    cartCounterLabel.innerHTML = ++cartCounter;
+    cartCounterLabel.innerHTML = ++cartCounter + '';
 
-    if (cartCounter === 1) {
-      cartCounterLabel.style.display = 'block'
-    }
+    if (cartCounter === 1) cartCounterLabel.style.display = 'block';
 
-    target.disabled = true;  // деактивации кнопки - альтернатива снятию события
-
-    let tempButton = target;
     let restoreHTML = target.innerHTML;
-
     target.innerHTML = fnPriceCounter(target);
 
-    setTimeout(fnRestore, 2000, tempButton, restoreHTML);
+    setTimeout(fnRestore, 2000, target, restoreHTML);
   }
 };
 
