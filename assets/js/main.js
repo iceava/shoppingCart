9dc80f0d9daa2bd1e807ff91b1b04327d12884c5
Andrey Fomenko <fomenkoandrey1978@gmail.com>
2019-04-18 11:24:31 +0300

    Отступы 4-->2.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,30 +4,30 @@ let cartCounterLabel = document.querySelector('#cart-counter');
 let buttonsContainer = document.querySelector('.page-content');
 
 let btnClickHandler = (e) => {
-    let target = e.target;
+  let target = e.target;
 
-    if (target.classList.contains('item-actions__cart')) {
+  if (target.classList.contains('item-actions__cart')) {
 
-        cartCounterLabel.innerHTML = `${++cartCounter}`;
-        if (cartCounter === 1) cartCounterLabel.style.display = 'block';
+    cartCounterLabel.innerHTML = `${++cartCounter}`;
+    if (cartCounter === 1) cartCounterLabel.style.display = 'block';
 
-        let restoreHTML = target.innerHTML;
+    let restoreHTML = target.innerHTML;
 
-        target.innerHTML = (() => {
-            let tempPrice = target.parentElement.previousElementSibling.innerHTML;
+    target.innerHTML = (() => {
+      let tempPrice = target.parentElement.previousElementSibling.innerHTML;
 
-            cartPrice += +tempPrice.replace(/^\$(\d+)\s\D+(\d+).*$/gu, '$1.$2');
+      cartPrice += +tempPrice.replace(/^\$(\d+)\s\D+(\d+).*$/gu, '$1.$2');
 
-            return `Added ${cartPrice.toFixed(2)} $`;
-        })();
+      return `Added ${cartPrice.toFixed(2)} $`;
+    })();
 
-        buttonsContainer.removeEventListener('click', btnClickHandler);
+    buttonsContainer.removeEventListener('click', btnClickHandler);
 
-        setTimeout((elem, restore) => {
-            elem.innerHTML = restore;
-            buttonsContainer.addEventListener('click', btnClickHandler);
-        }, 2000, target, restoreHTML);
-    }
+    setTimeout((elem, restore) => {
+      elem.innerHTML = restore;
+      buttonsContainer.addEventListener('click', btnClickHandler);
+    }, 2000, target, restoreHTML);
+  }
 };
 
 buttonsContainer.addEventListener('click', btnClickHandler);
