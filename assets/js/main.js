761b5ec3aadb85361d212de0b49ec10a4b53f9e6
Andrey Fomenko <fomenkoandrey1978@gmail.com>
2019-12-04 09:39:06 +0200

    Оптимизации кода.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,33 @@
+let buttonsContainer = document.querySelector('.page-content');
+let cartCounterLabel = document.querySelector('#cart-counter');
 let cartCounter = 0;
 let cartPrice = 0;
-let cartCounterLabel = document.querySelector('#cart-counter');
-let buttonsContainer = document.querySelector('.page-content');
 
 let btnClickHandler = (e) => {
   let target = e.target;
 
   if (target.classList.contains('item-actions__cart')) {
+    cartCounterLabel.innerHTML = ++cartCounter;
 
-    cartCounterLabel.innerHTML = `${++cartCounter}`;
     if (cartCounter === 1) cartCounterLabel.style.display = 'block';
+
+    const mockData = +target.parentElement.previousElementSibling.innerHTML.replace(
+      /^\$(\d+)\s\D+(\d+).*$/gu,
+      '$1.$2'
+    );
+
+    cartPrice = Math.round((cartPrice + mockData) * 100) / 100;
 
     let restoreHTML = target.innerHTML;
 
-    target.innerHTML = (() => {
-      const mockData = target.parentElement.previousElementSibling.innerHTML;
-
-      cartPrice += Math.round(+mockData.
-        replace(/^\$(\d+)\s\D+(\d+).*$/gu, '$1.$2') * 100) / 100;
-
-      return `Added ${cartPrice.toFixed(2)} $`;
-    })();
+    target.innerHTML = `Added ${cartPrice.toFixed(2)} $`;
 
     buttonsContainer.removeEventListener('click', btnClickHandler);
 
-    setTimeout((elem, restore) => {
-      elem.innerHTML = restore;
+    setTimeout(() => {
+      target.innerHTML = restoreHTML;
       buttonsContainer.addEventListener('click', btnClickHandler);
-    }, 2000, target, restoreHTML);
+    }, 2000);
   }
 };
 
